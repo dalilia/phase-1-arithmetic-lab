5217a786d2ebe82ed9142e8d3fd8b9277b463962
dalilia <dalila200875yahoo.fr>
2023-07-22 15:10:34 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+// Create the multiply variable using the multiplication equation
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 100); // Generates a random integer between 1 and 100 (inclusive)
+
+
+let num3;
+let num4;
+
+num4 = 6;
+num3 = 10;
+
+const mod = num3 % num4;
+
+
+const numberSet = [10, 5, 20, 15, 3];
+const max = Math.max(numberSet);
